f1cd8615a30e16e8506d9f7ec0676d460072b823
Elsner <Elsner@EBGO>
2022-02-25 18:08:15 -0600

    capa de datos user...Elsner

---
+++ Security/src/main/java/datos/Dt_usuario.java
@@ -0,0 +1,76 @@
+package datos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import entidades.Tbl_user;
+
+public class Dt_usuario {
+	
+	//Atributos
+	poolConexion pc = poolConexion.getInstance(); 
+	Connection c = null;
+	private ResultSet rsUsuario = null;
+	private ResultSet rs = null;
+	private PreparedStatement ps = null;
+	
+	//Metodo para llenar el RusultSet //para insert, update and delete
+	public void llenaRsUsuario(Connection c){
+		try{
+			ps = c.prepareStatement("SELECT * FROM seguridad.tbl_user;", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE, ResultSet.HOLD_CURSORS_OVER_COMMIT);
+			rsUsuario = ps.executeQuery();
+		}
+		catch (Exception e){
+			System.out.println("DATOS: ERROR EN LISTAR USUARIOS "+ e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	//Metodo para visualizar usuarios registrados y activos
+	public ArrayList<Tbl_user> listaUserActivos(){
+		ArrayList<Tbl_user> listUser = new ArrayList<Tbl_user>();
+		try{
+			c = poolConexion.getConnection(); //obtenemos una conexion del pool
+			ps = c.prepareStatement("SELECT * FROM seguridad.tbl_user WHERE estado<>3;", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = ps.executeQuery();
+			while(rs.next()){
+				Tbl_user user = new Tbl_user(); //instanciamos a usuario
+				user.setId_user(rs.getInt("id_user"));
+				user.setUser(rs.getString("user"));
+				user.setPwd(rs.getString("pwd"));
+				user.setNombres(rs.getString("nombres"));
+				user.setApellidos(rs.getNString("apellidos"));
+				/*user.setfCreacion(rs.getTimestamp("fcreacion"));*/
+				user.setEstado(rs.getInt("estado"));
+				listUser.add(user);
+			}
+		}
+		catch (Exception e){
+			System.out.println("DATOS: ERROR EN LISTAR USUARIOS "+ e.getMessage());
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				if(rs != null){
+					rs.close();
+				}
+				if(ps != null){
+					ps.close();
+				}
+				if(c != null){
+					poolConexion.closeConnection(c);
+				}
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		return listUser;
+	}
+	
+	
+
+}
